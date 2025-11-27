70143c9468db7856c780a99275eb11fd19ab06bd
ming <oterges00@gmail.com>
2025-11-27 19:40:11 +0900

    .

---
+++ msa-demo/orchestration/src/test/java/com/example/orchestration/OrchestrationApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationApplicationTests {
+
+}
